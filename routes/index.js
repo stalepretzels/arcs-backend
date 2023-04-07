b0ac88b63616ec211d14ac105de997380421385d
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2023-04-07 07:24:14 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const address = require('address');
 
 router.get("/", function (req, res) {
     if (req.cookies.user == undefined) {
     res.cookie(`user`, "@guest" + Math.floor(Math.random() * 999999999));
     user = req.cookies.user;
+    console.log('User ' + req.cookies.user + ' joined from ' + address.ip());
     } else {
         user = req.cookies.user;
+        console.log('User ' + req.cookies.user + ' joined from ' + address.ip());
     }
     res.render("index");
 });
