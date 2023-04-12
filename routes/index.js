74203876800ca427dddb30535d3a13a9ce087b53
Clover Johnson <130152029+callmeclover-alt@users.noreply.github.com>
2023-04-12 17:32:46 +0000

    added automod and started PERSIST

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,10 @@ router.get("/", function (req, res) {
     if (req.cookies.user == undefined) {
     res.cookie(`user`, "@guest" + Math.floor(Math.random() * 999999999));
     user = req.cookies.user;
-    console.log('User ' + user + ' joined from ' + address.ip());
+    res.cookie('ip', address.ip());
     } else {
         user = req.cookies.user;
-        console.log('User ' + user + ' joined from ' + address.ip());
+        res.cookie('ip', address.ip());
     }
     if (req.cookies.uname == undefined) {
         } else {
--- server.js
+++ server.js
@@ -12,6 +12,58 @@ const md = require('markdown-it')('commonmark', {
   typographer: true
 });
 const striptags = require('striptags');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// MongoDB init (for persist)                                                                                                                                 
+const uri = "mongodb+srv://callmeclover:1322454676909BnM@cluster0.yyv5lxs.mongodb.net/?retryWrites=true&w=majority";
+const dbClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const dbName = "Messages";
+                      
+async function send(data) {
+   try {
+        await dbClient.connect();
+        console.log("Connected correctly to server, attempting to send message now...");
+        const db = dbClient.db(dbName);
+        const col = db.collection("Messages");
+
+        var object = {
+          message: data
+        };
+
+        // Insert a single document, wait for promise so we can read it back
+        const p = await col.insertOne(object);
+        // Find one document
+        console.log('Success.')
+       } catch (err) {
+        console.log(err.stack);
+    }
+
+  /*  finally {
+       await dbClient.close();
+   } */
+}
+
+async function get(client) {
+  try {
+       await dbClient.connect();
+       console.log("Connected correctly to server, attempting to catch messages now...");
+       const db = dbClient.db(dbName);
+       const col = db.collection("Messages");
+
+      var array = col.find().toArray();
+      for (var i in array) {
+        client.emit('broad', i)
+        console.log(i)
+      }
+       console.log('Success.')
+      } catch (err) {
+       console.log(err.stack);
+   }
+
+ /*  finally {
+      await dbClient.close();
+  } */
+}
 
 // Routes
 var index = require("./routes/index");
@@ -21,10 +73,12 @@ var fot = require("./routes/errors/403");
 var fof = require("./routes/errors/404");
 var fho = require("./routes/errors/500");
 var i2 = require("./routes/index2");
+var bpbtpage = require("./routes/bopbot");
 
 // Declarations
-const PORT = 8080;
-const BLOCKLIST = ['/a', '/b', '/c'];
+const PORT = 5000;
+const BLOCKLIST = [];
+const BOPBOTBANNEDWORDS = ['you are gay', 'i love methamphetamine', 'i love heroin', 'i love drugs', 'i love crack', 'i love cocaine'];
 
 // App init
 const app = express();
@@ -32,7 +86,8 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, { maxHttpBufferSize: 1e7 });
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.use(express.json());
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static('../node_modules'));
 
@@ -42,16 +97,28 @@ io.on('connection', function (client) {
 
   client.on('join', function (data) {
     console.log(data);
+    get(client).catch(console.dir);
+  });
+
+  client.on('joinEmit', function (data) {
+    client.broadcast.emit('broad', "<div id='joinmsg'>" + data + "</div>")
+    client.emit('broad', "<div id='joinmsg'>You joined the chat room.</div>")
   });
 
   client.on('messages', function (data) {
-      if (data.image == "ajsdgkjbdkfjgbijk") {
+    if (BOPBOTBANNEDWORDS.some(function(v) { return data.text.indexOf(v) >= 0; })) {
+    client.emit('broad', "<div class='chmscon'><strong>BopBot:</strong><div class='chat-msg bopbot'>You've been bopped! This is because your message contained banned words. Your message was not sent. Only you can see this message. Click <a style='text-decoration: wavy underline; color: inherit;' href='/bopbot'>here</a> to learn more.</div></div>")
+    } else {
+      if (data.image == null) {
         client.emit('broad', "<div class='chmscon'><strong>" + data.disName + " (" + data.genName + "):</strong><div class='chat-msg user'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "</div></div>");
         client.broadcast.emit('broad', "<div class='chmscon'><strong>" + data.disName + " (" + data.genName + "):</strong><div class='chat-msg other'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "</div></div>");
+        send("<div class='chmscon'><strong>" + data.disName + " (" + data.genName + "):</strong><div class='chat-msg other'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "</div></div>").catch(console.dir);
       } else {
         client.emit('broad', "<div class='chmscon'><strong>" + data.disName + " (" + data.genName + "):</strong><div class='chat-msg user'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "<br><img class='sentImage' src='" + data.image + "'>" + "</div></div>");
       client.broadcast.emit('broad', "<div class='chmscon'><strong>" + data.disName + " (" + data.genName + "):</strong><div class='chat-msg other'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "<br><img class='sentImage' src='" + data.image + "'>" + "</div></div>");
+      send("<div class='chmscon'><strong>" + data.disName + " (" + data.genName + "):</strong><div class='chat-msg other'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "<br><img class='sentImage' src='" + data.image + "'>" + "</div></div>").catch(console.dir);
       }
+    }
   });
 
 });
@@ -64,7 +131,6 @@ const c403 = function (req, res, next) {
       res.render('errors/error403');
     }
   })
-  console.log('Checked');
   next();
 }
 
@@ -77,9 +143,9 @@ app.use("/", index);
 app.use("/test", test);
 app.use("/401", foo);
 app.use("/403", fot);
-app.use("*", fof);
+// app.use("*", fof);
 app.use("/500", fho);
-
+app.use("/bopbot", bpbtpage);
 
 httpServer.listen(PORT, function (err) {
   if (err) console.error("Error in server setup process");
