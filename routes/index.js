1228b02859e9914221dbff8894ffedf1613774e4
Clover Johnson <130152029+callmeclover-alt@users.noreply.github.com>
2023-04-16 15:35:06 +0000

    Push from codespace

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,51 @@
 const express = require("express");
 const router = express.Router();
 const address = require('address');
+const avatarIcon = require('avatar-icon')
+var cookie = require('cookie-parse');
+const Chance = require('chance');
+var chance = new Chance();
+
+const BANNEDIPS = [];
 
 router.get("/", function (req, res) {
     if (req.cookies.user == undefined) {
-    res.cookie(`user`, "@guest" + Math.floor(Math.random() * 999999999));
+    res.cookie(`user`, "@guest" + Math.floor(Math.random() * 999999999), { secure: true });
     user = req.cookies.user;
-    res.cookie('ip', address.ip());
+    res.cookie('ip', address.ip(), { secure: true });
     } else {
         user = req.cookies.user;
-        res.cookie('ip', address.ip());
+        res.cookie('ip', address.ip(), { secure: true });
     }
     if (req.cookies.uname == undefined) {
         } else {
             user = req.cookies.user;
         }
+        if (req.cookies.pfp == undefined || req.cookies.pfp == null) {
+            res.cookie('pfp', avatarIcon({
+                size            : 72,   // px
+                density         : 10,   // max amount of shapes in one row
+                colorRange      : chance.integer({min : 6, max : 12}),   // amount of different colors
+                brightness      : 40,   // make it bright: start at 40 from 255 colors
+                contrast        : 50,   // %, take similar colors
+                backgroundColor : '',   // #hex or empty
+                fillRatio       : chance.integer({min : 40, max : 60}),   // %, some white space
+                rectangleRatio  : chance.integer({min : 40, max : 60}),   // %, ratio of rectangles
+                triangleRatio   : chance.integer({min : 20, max : 40}),   // %, ratio of triangles
+                circleRatio     : chance.integer({min : 10, max : 20}),    // %, ratio of circles
+                returnType      : '',   // default dataURL || 'buffer'
+              }));
+            pfp = req.cookies.pfp;
+        } else {
+            pfp = req.cookies.pfp;
+        }
+    room = room
+    if (BANNEDIPS.some(function(v) { return address.ip().toString().indexOf(v) >= 0; })) {
+        res.render('errors/banned.ejs')
+        console.log('User from ' + address.ip() + ' banned.')
+    } else {
         res.render('index');
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,60 +12,10 @@ const md = require('markdown-it')('commonmark', {
   typographer: true
 });
 const striptags = require('striptags');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { createAdapter } = require("@socket.io/mongo-adapter");
+const { MongoClient } = require("mongodb");
+const parseDataURL = require("data-urls");
 
-// MongoDB init (for persist)                
-/*                                                                                                                 
-const uri = "mongodb+srv://callmeclover:1322454676909BnM@cluster0.yyv5lxs.mongodb.net/?retryWrites=true&w=majority";
-const dbClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const dbName = "Messages";
-                      
-async function send(data) {
-   try {
-        await dbClient.connect();
-        console.log("Connected correctly to server, attempting to send message now...");
-        const db = dbClient.db(dbName);
-        const col = db.collection("Messages");
-
-        var object = {
-          message: data
-        };
-
-        // Insert a single document, wait for promise so we can read it back
-        const p = await col.insertOne(object);
-        // Find one document
-        console.log('Success.')
-       } catch (err) {
-        console.log(err.stack);
-    }
-
-  finally {
-       await dbClient.close();
-   }
-  }
-
-async function get(client) {
-  try {
-       await dbClient.connect();
-       console.log("Connected correctly to server, attempting to catch messages now...");
-       const db = dbClient.db(dbName);
-       const col = db.collection("Messages");
-
-      var array = col.find().toArray();
-      for (var i in array) {
-        client.emit('broad', i)
-        console.log(i)
-      }
-       console.log('Success.')
-      } catch (err) {
-       console.log(err.stack);
-   }
-
-    finally {
-      await dbClient.close();
-  } 
-}
-*/
 // Routes
 var index = require("./routes/index");
 var test = require("./routes/test");
@@ -75,11 +25,14 @@ var fof = require("./routes/errors/404");
 var fho = require("./routes/errors/500");
 var i2 = require("./routes/index2");
 var bpbtpage = require("./routes/bopbot");
+const { Socket } = require("socket.io-client");
 
 // Declarations
-const PORT = 5000;
+const PORT = 8080;
 const BLOCKLIST = [];
-const BOPBOTBANNEDWORDS = ['you are gay', 'i love methamphetamine', 'i love heroin', 'i love drugs', 'i love crack', 'i love cocaine'];
+const BOPBOTBANNEDWORDS = ['you are gay', 'methamphetamine', 'heroin', 'drug', 'crack', 'cocaine'];
+var ROOM = '::GENERAL'
+room = ROOM
 
 // App init
 const app = express();
@@ -95,10 +48,12 @@ app.use(express.static('../node_modules'));
 // Socket.io init
 io.on('connection', function (client) {
   console.log('Client connected...');
+  client.join(ROOM); 
+  ROOM = client.rooms[0]
 
   client.on('join', function (data) {
     console.log(data);
-   // get(client).catch(console.dir);
+    get(client).catch(console.dir);
   });
 
   client.on('joinEmit', function (data) {
@@ -106,18 +61,45 @@ io.on('connection', function (client) {
     client.emit('broad', "<div id='joinmsg'>You joined the chat room.</div>")
   });
 
+  client.on('joinRoom', (data) => {
+    client.join(data.rtj);
+//    client.to(data.rtj).broadcast.emit('broad', 'user joined (test)')
+    ROOM = client.rooms[0]
+  })
+
+  client.on("disconnected", (data) => {
+    console.log(data);
+  });
+
+  client.on('imageUpdate', (data) => {
+   console.log(data)
+  })
+
+  client.on("disconnectEmit", (data) => {
+    client.broadcast.emit('broad', "<div id='joinmsg'>" + data + "</div>")
+  });
+
   client.on('messages', function (data) {
     if (BOPBOTBANNEDWORDS.some(function(v) { return data.text.indexOf(v) >= 0; })) {
     client.emit('broad', "<div class='chmscon'><strong>BopBot:</strong><div class='chat-msg bopbot'>You've been bopped! This is because your message contained banned words. Your message was not sent. Only you can see this message. Click <a style='text-decoration: wavy underline; color: inherit;' href='/bopbot'>here</a> to learn more.</div></div>")
     } else {
       if (data.image == null) {
-        client.emit('broad', "<div class='chmscon'><strong>" + data.disName + " (" + data.genName + "):</strong><div class='chat-msg user'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "</div></div>");
-        client.broadcast.emit('broad', "<div class='chmscon'><strong>" + data.disName + " (" + data.genName + "):</strong><div class='chat-msg other'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "</div></div>");
-        send("<div class='chmscon'><strong>" + data.disName + " (" + data.genName + "):</strong><div class='chat-msg other'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "</div></div>").catch(console.dir);
+      /*
+
+      CHEATSHEET:
+
+      client.emit('EVENT', DATA)
+      send to user
+      client.to(ROOM).emit('EVENT', DATA)
+      broadcast
+
+      */
+
+        client.emit('broad', "<div class='chmscon'><div id='msghead' style='margin-bottom: 5px;'><img src='" + data.pfp + "' style='border-radius: 2em;width: 40px;margin-right: 10px;'><strong>" + data.disName + " (" + data.genName + "):</strong></div><div class='chat-msg user'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "</div></div>");
+        client.to(client.rooms[0]).emit('broad', "<div class='chmscon'><div id='msghead' style='margin-bottom: 5px;'><img src='" + data.pfp + "' style='border-radius: 2em;width: 40px;margin-right: 10px;'><strong>" + data.disName + " (" + data.genName + "):</strong></div><div class='chat-msg other'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "</div></div>");
       } else {
-        client.emit('broad', "<div class='chmscon'><strong>" + data.disName + " (" + data.genName + "):</strong><div class='chat-msg user'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "<br><img class='sentImage' src='" + data.image + "'>" + "</div></div>");
-      client.broadcast.emit('broad', "<div class='chmscon'><strong>" + data.disName + " (" + data.genName + "):</strong><div class='chat-msg other'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "<br><img class='sentImage' src='" + data.image + "'>" + "</div></div>");
-      send("<div class='chmscon'><strong>" + data.disName + " (" + data.genName + "):</strong><div class='chat-msg other'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "<br><img class='sentImage' src='" + data.image + "'>" + "</div></div>").catch(console.dir);
+        client.emit('broad', "<div class='chmscon'><div id='msghead' style='margin-bottom: 5px;'><img src='" + data.pfp + "' style='border-radius: 2em;width: 40px;margin-right: 10px;'><strong>" + data.disName + " (" + data.genName + "):</strong></div><div class='chat-msg user'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "<br><img class='sentImage' src='" + data.image + "'>" + "</div></div>");
+        client.to(client.rooms[0]).emit('broad', "<div class='chmscon'><div id='msghead' style='margin-bottom: 5px;'><img src='" + data.pfp + "' style='border-radius: 2em;width: 40px;margin-right: 10px;'><strong>" + data.disName + " (" + data.genName + "):</strong></div><div class='chat-msg other'>" + striptags(md.render(data.text), ['strong', 'i', 'em', 'code', 'a', 'div', 'sub', 'sup', 's']) + "<br><img class='sentImage' src='" + data.image + "'>" + "</div></div>");
       }
     }
   });
