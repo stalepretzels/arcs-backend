be02409db49b9d2e9578eadcab3ae49921da4e61
Clover Johnson <130152029+callmeclover-alt@users.noreply.github.com>
2023-05-01 16:46:11 +0000

    quick fix 2

--- server.js
+++ server.js
@@ -40,7 +40,6 @@ app.set('view engine', 'ejs');
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static('../node_modules'));
 
 // Socket.io init
 io.on('connection', function (client) {
