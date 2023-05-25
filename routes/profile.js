b88cf547d71fab857dfed7fd1cd6a432d7e8a83c
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2023-05-25 12:41:24 +0000

    Add profiles, add mini-profiles next

---
+++ routes/profile.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const avatarIcon = require('avatar-icon')
+
+router.get("/", function (req, res) {
+  pfp = req.query.pfp
+  disName = req.query.disName
+  genName = req.query.genName
+  bio = req.query.bio
+  res.render("profile.ejs");
+});
+
+module.exports = router;
