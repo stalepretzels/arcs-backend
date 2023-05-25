5f92f1053187eff43a132edf0e5395ed31bbdbe7
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2023-05-25 13:02:13 +0000

    added badges to some profiles, ask me how to get them

--- routes/profile.js
+++ routes/profile.js
@@ -7,6 +7,13 @@ router.get("/", function (req, res) {
   disName = req.query.disName
   genName = req.query.genName
   bio = req.query.bio
+
+  if (req.query.disName == 'callmeclover') {
+    creatorbadge = true
+    modbadge = true
+    bugbadge = true
+  }
+
   res.render("profile.ejs");
 });
 
