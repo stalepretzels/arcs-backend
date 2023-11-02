792eb0a7c7a7fbc4045aaa7c6649790e329eea32
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2023-11-02 12:49:23 -0400

    Update universal.js

--- public/assets/universal.js
+++ public/assets/universal.js
@@ -3,3 +3,16 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/serviceworker.js');
   });
 }
+
+        var onltext = document.getElementById("connected");
+        let onlinechecker = setInterval(() => {
+          if (socket.connected == true) {
+            onltext.textContent = '🗲'
+            onltext.style.color = 'gold'
+            onltext.setAttribute("tooltip", "You are connected to the server.");
+          } else if (socket.connected == false) {
+            onltext.textContent = '!'
+            onltext.style.color = 'red'
+            onltext.setAttribute("tooltip", "You are not connected to the server.");
+          }
+        }, 1500);
