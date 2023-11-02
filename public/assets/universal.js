2876f030c947b26bbf6374b03cc56af3f60e75a6
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2023-11-02 12:58:09 -0400

    Update universal.js

--- public/assets/universal.js
+++ public/assets/universal.js
@@ -7,8 +7,8 @@ if ('serviceWorker' in navigator) {
         var onltext = document.getElementById("connected");
         let onlinechecker = setInterval(() => {
           if (socket.connected == true) {
-            onltext.textContent = '🗲'
-            onltext.style.color = 'gold'
+            onltext.textContent = '☁'
+            onltext.style.color = 'chartreuse'
             onltext.setAttribute("tooltip", "You are connected to the server.");
           } else if (socket.connected == false) {
             onltext.textContent = '!'
