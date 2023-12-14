dce75c50c1e54e841f7eb8c5b124e0dc38cb3836
Clover Johnson <callmeclover.trs@gmail.com>
2023-12-14 14:58:03 -0500

    fhedfbsdfg

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const striptags = require("striptags");
+const compression = require("compression");
 const { instrument } = require("@socket.io/admin-ui");
 
 const markdown = require("markdown-it")({
@@ -22,7 +23,6 @@ const markdown = require("markdown-it")({
   .use(require("markdown-it-sup")) // these
   .use(require("markdown-it-mark")) // it
   .use(require("markdown-it-anchor")) // looks
-  .use(require("markdown-it-highlightjs")); // cool
 
 // Declarations
 let app = express();
@@ -51,8 +51,44 @@ let routes = {
 };
 
 // Functions
+function cleanseMessage(message) {
+    return striptags(markdown.render(striptags(`<pre style="font-family:  'Inter Tight', sans-serif; white-space: nowrap; margin: 0;">${message}</pre>`, ['pre'])), [
+            'strong',
+            'i',
+            'em',
+            'code',
+            'a',
+            'div',
+            'sub',
+            'sup',
+            's',
+            'abbr',
+            'dl',
+            'hr',
+            'ins',
+            'section',
+            'ol',
+            'li',
+            'sup',
+            'sub',
+            'code',
+            'pre',
+            'br',
+            'h1',
+            'h2',
+            'h3','h4','h5','h6'
+          ])
+}
+
+function createMessage(username, message, bio) {
+  let msg =
+  `<div style='margin: 10px 0;'><span><span class='pfplink' onclick='window.location = "/profile?user=${username}&bio=${bio}"'>${username} </span><span style='font-size: small;'>[at <span id='date'></span>]</span></span><div id='message'>${cleanseMessage(message)}</div></div>`
+
+  return msg;
+}
 
 // Initialization
+app.use(compression())
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -66,162 +102,67 @@ app.use("/", routes.login);
 app.use("/", routes.extras);
 
 // Socket.io
-io.on("connection", (client) => {
-  console.log("Client connected...");
-  client.join("::GENERAL");
+io.on('connection', (client) => {
+  console.log('Client connected...')
+  client.join('::GENERAL')
 
-  client.on("join", function (data) {
-    if (!(data.user.disName == undefined)) { 
-    if (!(data.preroom == '')) {
-    client.leave(data.preroom);
-    client.join(data.room);
-    client
-      .to(data.room)
-      .emit(
-        "broad",
-        "<div class='statusmsg'>" +
-          data.user.disName +
-          "@" +
-          data.user.ugn +
-          " joined this chat room.</div>",);
-        client
-      .to(data.preroom)
-      .emit(
-        "broad",
-        "<div class='statusmsg'>" +
-          data.user.disName +
-          "@" +
-          data.user.ugn +
-          " left this chat room.</div>",
-      );
-    client.emit(
-      "broad",
-      "<div class='statusmsg'>You joined " + data.room + ".</div>",
-    );
-    } else {
-    console.log(data.user.disName + "@" + data.user.ugn + " joined.");
-    client
-      .to("::GENERAL")
-      .emit(
-        "broad",
-        "<div class='statusmsg'>" +
-          data.user.disName +
-          "@" +
-          data.user.ugn +
-          " joined this chat room.</div>",
-      );
-    client.emit(
-      "broad",
-      "<div class='statusmsg'>You joined the chat room.</div>",
-    );
-    }
-  }});
-
-  client.on("connect_error", (err) => {
-    console.log(`connect_error due to ${err.message}`);
-  });
-
-  client.on('command', (data)=>{
-    switch (data.type) {
-      case 'ping':
-        data.callback()
-        break;
-      default:
-        client.emit('broad', "<div id='statusmsg'>Invalid command</div>")
-    }
-  });
-  
-  client.on("messages", (data) => {
+  client.on('join', function (data) {
     if (!(data.user.disName == undefined)) {
-    client.emit(
-      "broad",
-      "<div style='margin: 10px 0;'><span><span class='pfplink' onclick='window.location = \'" + `/profile?user=${data.user.disName}@${data.user.ugn}&bio=${data.user.bio}` + "'>" +
-      data.user.disName +
-      "@" +
-      data.user.ugn +
-      "</span> <span style='font-size: small;'>[at " +
-        "<span id='date'></span>" +
-        "]</span></span><div id='message'>" +
-        striptags(markdown.render(striptags(data.message)), [
-          "strong",
-          "i",
-          "em",
-          "code",
-          "a",
-          "div",
-          "sub",
-          "sup",
-          "s",
-          "abbr",
-          "dl",
-          "hr",
-          "ins",
-          "section",
-          "ol",
-          "li",
-          "sup",
-          "sub",
-          "code",
-          "pre",
-          "br",
-          "h1",
-          "h2",
-          "h3",
-          "h4",
-          "h5",
-          "h6",
-        ]) +
-        "</div></div>",
-    );
-    client
-      .to(data.room)
-      .emit(
-        "notification", data
+      if (!(data.preroom == '')) {
+        client.leave(data.preroom)
+        client.join(data.room)
+        client
+          .to(data.room)
+          .emit(
+            'broad',
+            `<div class='statusmsg'>${data.user.disName}@${data.user.ugn} joined this chat room.</div>`
+          )
+        client
+          .to(data.preroom)
+          .emit(
+            'broad',
+            `<div class='statusmsg'>${data.user.disName}@${data.user.ugn}left this chat room.</div>`
+          )
+        client.emit(
+          'broad',
+          `<div class='statusmsg'>You joined ${data.room}.</div>`
+        )
+      } else {
+        console.log(
+          `${data.user.disName}@${data.user.ugn} joined.`
+        )
+        client
+          .to('::GENERAL')
+          .emit(
+            'broad',
+            `<div class='statusmsg'>${data.user.disName}@${data.user.ugn} joined this chat room.</div>`
+          )
+        client.emit(
+          'broad',
+          "<div class='statusmsg'>You joined the chat room.</div>"
+        )
+      }
+    }
+  })
+
+  client.on('connect_error', (err) => {
+    console.log(`connect_error due to ${err.message}`)
+  })
+
+  client.on('messages', (data) => {
+    if (!(data.user.disName == undefined)) {
+      client.emit(
+        'broad', createMessage(data.user.disName, data.message, data.user.bio)
       )
-    client
-      .to(data.room)
-      .emit(
-        "broad",
-        "<div style='margin: 10px 0;'><span><span class='pfplink' onclick='window.location = \"" + `/profile?user=${data.user.disName}@${data.user.ugn}&bio=${data.user.bio}` + "\"'>" +
-          data.user.disName +
-          "@" +
-          data.user.ugn +
-          "</span> <span style='font-size: small;'>[at " +
-          "<span id='date'></span>" +
-          "]</span></span><div id='message'>" +
-          striptags(markdown.render(striptags(data.message)), [
-            "strong",
-            "i",
-            "em",
-            "code",
-            "a",
-            "div",
-            "sub",
-            "sup",
-            "s",
-            "abbr",
-            "dl",
-            "hr",
-            "ins",
-            "section",
-            "ol",
-            "li",
-            "sup",
-            "sub",
-            "code",
-            "pre",
-            "br",
-            "h1",
-            "h2",
-            "h3",
-            "h4",
-            "h5",
-            "h6",
-          ]) +
-          "</div></div>",
-      );
-  }});
-});
+      client.to(data.room).emit('notification', data)
+      client
+        .to(data.room)
+        .emit(
+          'broad', createMessage(data.user.disName, data.message, data.user.bio)
+        )
+    }
+  })
+})
 
 httpServer.listen(8443, () => {
   console.log("running arcs at localhost:8443");
