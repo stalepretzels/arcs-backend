9fbd9890e39dc1c3baeadb68f9d31bdbd71775d2
Clover Johnson <callmeclover.trs@gmail.com>
2023-12-17 16:04:56 -0500

    whitespace no worky

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ function cleanseMessage(message) {
             'br',
             'h1',
             'h2',
-            'h3','h4','h5','h6'
+            'h3','h4','h5','h6', 'br'
           ])}</pre>`
 }
 
