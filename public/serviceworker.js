4c4d871cf60ed677a546438ee30419706424ad22
User Callmeclover <callmeclover@s1.serv00.com>
2024-01-02 21:24:28 +0100

    wah wah

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,10 +1,10 @@
 const addResourcesToCache = async (resources) => {
-  const cache = await caches.open("v1");
+  const cache = await caches.open('v1');
   await cache.addAll(resources);
 };
 
 const putInCache = async (request, response) => {
-  const cache = await caches.open("v1");
+  const cache = await caches.open('v1');
   await cache.put(request, response);
 };
 
@@ -13,78 +13,89 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
   const responseFromCache = await caches.match(request);
   if (responseFromCache) {
     return responseFromCache;
-}
-
-// Next try to use (and cache) the preloaded response, if it's there
-const preloadResponse = await preloadResponsePromise;
-if (preloadResponse) {
-  console.info("using preload response", preloadResponse);
-  putInCache(request, preloadResponse.clone());
-  return preloadResponse;
-}
-
-// Next try to get the resource from the network
-try {
-  const responseFromNetwork = await fetch(request);
-  // response may be used only once
-  // we need to save clone to put one copy in cache
-  // and serve second one
-  putInCache(request, responseFromNetwork.clone());
-  return responseFromNetwork;
-} catch (error) {
-  const fallbackResponse = await caches.match(fallbackUrl);
-  if (fallbackResponse) {
-    return fallbackResponse;
   }
-  // when even the fallback response is not available,
-  // there is nothing we can do, but we must always
-  // return a Response object
-  return new Response("Network error happened", {
-    status: 408,
-    headers: { "Content-Type": "text/plain" },
-  });
-}
+
+  // Next try to use the preloaded response, if it's there
+  const preloadResponse = await preloadResponsePromise;
+  if (preloadResponse) {
+    console.info('using preload response', preloadResponse);
+    putInCache(request, preloadResponse.clone());
+    return preloadResponse;
+  }
+
+  // Next try to get the resource from the network
+  try {
+    const responseFromNetwork = await fetch(request.clone());
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+    // when even the fallback response is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object
+    return new Response('Network error happened', {
+      status: 408,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
 };
 
-// Enable navigation preload
 const enableNavigationPreload = async () => {
-if (self.registration.navigationPreload) {
-  await self.registration.navigationPreload.enable();
-}
+  if (self.registration.navigationPreload) {
+    // Enable navigation preloads!
+    await self.registration.navigationPreload.enable();
+  }
 };
 
-self.addEventListener("activate", (event) => {
-event.waitUntil(enableNavigationPreload());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(enableNavigationPreload());
 });
 
-self.addEventListener("install", (event) => {
-event.waitUntil(
-  addResourcesToCache([
-      "/main.ejs",
-      "/profile.ejs",
-      "/chat.ejs",
-      "/editinfo.ejs",
-      "/rules.ejs",
-      "/about.ejs",
-      "/partials/header.ejs",
-      "/css/main.css",
-      "/app.js",
-      "/assets/jquery.js",
-      "/assets/socket.io.js", 
-      "/logo.png",
-      "/maskable.png",
-      "/favicon.ico",
-      "/error.ejs"
-  ]),
-);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      /*'/',
+  '/chat',
+  '/edit',
+  '/error',
+  '/profile',
+  '/about',
+  '/rules',  */
+    'https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+   
+  './assets/socket.io.js',
+  './assets/universal.js',
+      './assets/chance.min.js',
+      './assets/jquery.js',
+    './assets/arrive.min.js',
+  
+  './img/logo.png',
+  './img/favicon.ico',
+      './img/maskable.png',
+  './img/android-chrome-192x192.png',
+  './img/android-chrome-384x384.png',
+    './img/apple-touch-icon.png',
+      './img/safari-pinned-tab.svg',
+      './img/favicon-16x16.png',
+    './img/favicon-32x32.png',
+      './img/mstile-150x150.png',
+  './css/main.css',
+    ])
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-event.respondWith(
-  cacheFirst({
-    request: event.request,
-    preloadResponsePromise: event.preloadResponse,
-    fallbackUrl: "/logo.png",
-  }),
-);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+      preloadResponsePromise: event.preloadResponse,
+      fallbackUrl: './img/logo.png',
+    })
+  );
 });
