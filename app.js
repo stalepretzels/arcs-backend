8d0a0850d839268794dcdbb4063061e22a56800f
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-01-08 11:59:18 -0500

    add <time> instead of <span>

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const fastify = require("fastify")();
 const fastifyIO = require("fastify-socket.io");
 const cors = require("@fastify/cors");
 const striptags = require("striptags");
-const fs = require('read-file');
-const path = require('path');
-const mysql = require('mysql');
-const jdenticon = require('jdenticon');
+const fs = require("read-file");
+const path = require("path");
+const mysql = require("mysql");
+const jdenticon = require("jdenticon");
 const markdown = require("markdown-it")({
   html: true,
   linkify: true,
@@ -22,125 +22,152 @@ const markdown = require("markdown-it")({
   .use(require("markdown-it-sup"))
   .use(require("markdown-it-mark"))
   .use(require("markdown-it-anchor"))
-  .use(require('markdown-it-link-attributes'), {
-  attrs: {
-    target: '_blank',
-  }
-})
+  .use(require("markdown-it-link-attributes"), {
+    attrs: {
+      target: "_blank",
+    },
+  })
   .use(require("markdown-it-task-lists"));
 
 // Declarations
-let version = fs.sync(path.join(__dirname, 'version.txt'))
-  .toString()
-  .trim();
-let lastUUIDts = "";
+let version = fs.sync(path.join(__dirname, "version.txt")).toString().trim();
 
 // Functions
 function cleanseMessage(message) {
-    return striptags(markdown.render(striptags(message)), [
-            'strong',
-            'i',
-            'em',
-            'code',
-            'a',
-            'div',
-            'sub',
-            'sup',
-            's',
-            'abbr',
-            'dl',
-            'hr',
-            'ins',
-            'section',
-            'ol',
-            'li',
-            'sup',
-            'sub',
-            'code',
-            'pre',
-            'br',
-            'h1',
-            'h2',
-            'h3','h4','h5','h6', 'ul', 'input'
-          ])
+  return striptags(markdown.render(striptags(message)), [
+    "strong",
+    "i",
+    "em",
+    "code",
+    "a",
+    "div",
+    "sub",
+    "sup",
+    "s",
+    "abbr",
+    "dl",
+    "hr",
+    "ins",
+    "section",
+    "ol",
+    "li",
+    "sup",
+    "sub",
+    "code",
+    "pre",
+    "br",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "ul",
+    "input",
+  ]);
 }
 
-function createMessage(username, message, bio) {
+function createMessage(username, message, bio, date) {
   let cleansedMessage = cleanseMessage(message);
   let formattedDiv;
-  let formattedUsername = `<a href='/profile/${encodeURIComponent(username[0] + "@" + username[1])}'>${username[0]}@${username[1]}</a>`;
-    let formattedDate = `<span style='font-size: small;'> [at <span id='date'></span>]</span>`;
-  let formattedMessage = `<div id='message'><pre>${cleansedMessage}</pre></div>`; 
-  formattedDiv = `<div style='margin: 10px 0;'><div class="userDisplay">${jdenticon.toSvg(username[2], 26)}<span>${formattedUsername}${formattedDate}</span></div>${formattedMessage}</div>`;
+  let formattedUsername = `<a href='/profile/${encodeURIComponent(
+    username[0] + "@" + username[1]
+  )}'>${username[0]}@${username[1]}</a>`;
+  let formattedDate = `<time datetime="${date.toISOString()}">${date.toLocaleString()}</time>`;
+  let formattedMessage = `<div id='message'><pre>${cleansedMessage}</pre></div>`;
+  formattedDiv = `<div style='margin: 10px 0;'><div class="userDisplay">${jdenticon.toSvg(
+    username[2],
+    26
+  )}<span>${formattedUsername} [at ${formattedDate}]</span></div>${formattedMessage}</div>`;
   return formattedDiv;
 }
 
 // Init
 fastify.register(fastifyIO, {
   cors: {
-    origin: "*"
-  }
+    origin: "*",
+  },
 });
 fastify.register(cors, {
-  origin: "*"
+  origin: "*",
 });
-fastify.register(
-  require('@fastify/compress'),
-  { global: true }
-);
+fastify.register(require("@fastify/compress"), { global: true });
 
 var connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
-                                                         password: process.env.DB_PASS
-});  
+  password: process.env.DB_PASS,
+});
 connection.connect();
 
 // GET Routes
-fastify.get('/api/version', async (request, reply) => {
+fastify.get("/api/version", async (request, reply) => {
   reply.send({ version });
 });
 
-// POST Routes      
-                                                         
-                                                         
+// POST Routes
+
 fastify.ready().then(() => {
-  
-fastify.io.on('connection', (client) => {
-  console.log('Client connected...')
-  client.join('::GENERAL')
+  fastify.io.on("connection", (client) => {
+    console.log("Client connected...");
+    client.join("::GENERAL");
 
-  client.on('join', function(data) {
-    if (data.user.disName !== undefined) {
-      if (data.preroom !== '') {
-        client.leave(data.preroom);
-        client.join(data.room);
-        client.to(data.room).emit('broad', `<div class='statusmsg'>${data.user.disName}@${data.user.ugn} joined this chat room.</div>`);
-        client.to(data.preroom).emit('broad', `<div class='statusmsg'>${data.user.disName}@${data.user.ugn} left this chat room.</div>`);
-        client.emit('broad', `<div class='statusmsg'>You joined ${data.room}.</div>`);
-      } else {
-        console.log(`${data.user.disName}@${data.user.ugn} joined.`);
-        client.to('::GENERAL').emit('broad', `<div class='statusmsg'>${data.user.disName}@${data.user.ugn} joined this chat room.</div>`);
-        client.emit('broad', "<div class='statusmsg'>You joined the chat room.</div>");
+    client.on("join", function (data) {
+      if (data.user.disName !== undefined) {
+        if (data.preroom !== "") {
+          client.leave(data.preroom);
+          client.join(data.room);
+          client
+            .to(data.room)
+            .emit(
+              "broad",
+              `<div class='statusmsg'>${data.user.disName}@${data.user.ugn} joined this chat room.</div>`
+            );
+          client
+            .to(data.preroom)
+            .emit(
+              "broad",
+              `<div class='statusmsg'>${data.user.disName}@${data.user.ugn} left this chat room.</div>`
+            );
+          client.emit(
+            "broad",
+            `<div class='statusmsg'>You joined ${data.room}.</div>`
+          );
+        } else {
+          console.log(`${data.user.disName}@${data.user.ugn} joined.`);
+          client
+            .to("::GENERAL")
+            .emit(
+              "broad",
+              `<div class='statusmsg'>${data.user.disName}@${data.user.ugn} joined this chat room.</div>`
+            );
+          client.emit(
+            "broad",
+            "<div class='statusmsg'>You joined the chat room.</div>"
+          );
+        }
       }
-    }
-  });
+    });
 
-  client.on('connect_error', (err) => {
-    console.error(`connect_error due to ${err.message}`)
-  })
+    client.on("connect_error", (err) => {
+      console.error(`connect_error due to ${err.message}`);
+    });
 
-  client.on('messages', (data) => {
-    if (data.user.disName !== undefined) {
-      const message = createMessage([data.user.disName, data.user.ugn, data.user.uuid], data.message, data.user.bio);
-      client.emit('broad', message);
-      client.to(data.room).emit('notification', data);
-      lastUUIDts = data.user.uuid;
-      client.to(data.room).emit('broad', message);
-    }
+    client.on("messages", (data) => {
+      if (data.user.disName !== undefined) {
+        const message = createMessage(
+          [data.user.disName, data.user.ugn, data.user.uuid],
+          data.message,
+          data.user.bio,
+          data.sent
+        );
+        client.emit("broad", message);
+        client.to(data.room).emit("notification", data);
+        client.to(data.room).emit("broad", message);
+      }
+    });
   });
-})
 });
-                                                         
-fastify.listen({ path: 'passenger', host: '127.0.0.1' });
+
+fastify.listen({ path: "passenger", host: "127.0.0.1" });
