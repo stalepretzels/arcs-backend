15b8198a7511c2de52488a56ba78a3959d88a612
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-03-19 11:56:12 -0400

    Update bartender.ex

--- lib/bartender.ex
+++ lib/bartender.ex
@@ -2,5 +2,8 @@ defmodule Bartender do
   use Rustler, otp_app: :arcs, crate: "bartender"
 
   # When your NIF is loaded, it will override this function.
-  def filter_profanity(_input), do: :erlang.nif_error(:nif_not_loaded)
+  def censor(_input), do: :erlang.nif_error(:nif_not_loaded)
+  def is_inappropriate(_input), do: :erlang.nif_error(:nif_not_loaded)
+  def is(_input, _filter_input), do: :erlang.nif_error(:nif_not_loaded)
+  def isnt(_input, _filter_input), do: :erlang.nif_error(:nif_not_loaded)
 end
