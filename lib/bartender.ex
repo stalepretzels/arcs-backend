3de5f71382702bf177e69aad753df19956bf177e
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-03-20 08:22:32 -0400

    Bartender is officially implemented!

--- lib/bartender.ex
+++
@@ -1,9 +0,0 @@
-defmodule Bartender do
-  use Rustler, otp_app: :arcs, crate: "bartender"
-
-  # When your NIF is loaded, it will override this function.
-  def censor(_input), do: :erlang.nif_error(:nif_not_loaded)
-  def is_inappropriate(_input), do: :erlang.nif_error(:nif_not_loaded)
-  def is(_input, _filter_input), do: :erlang.nif_error(:nif_not_loaded)
-  def isnt(_input, _filter_input), do: :erlang.nif_error(:nif_not_loaded)
-end
