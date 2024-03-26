e45093ffb3e3e31021cc4544305296d11823aa5e
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-03-26 12:00:27 -0400

    implement something of an actual chat system

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,17 @@ use axum::{
 };
 use axum_extra::TypedHeader;
 
-use std::borrow::Cow;
-use std::ops::ControlFlow;
-use std::{net::SocketAddr, path::PathBuf};
-use std::error::Error;
+use std::{borrow::Cow,
+    ops::ControlFlow,
+    net::SocketAddr,
+    path::PathBuf,
+    error::Error,
+    collections::HashSet,
+    sync::{Arc, Mutex},
+    sync::atomic::{AtomicUsize, Ordering}
+};
+use once_cell::sync::Lazy;
+use tokio::sync::broadcast;
 use tower_http::{
     services::ServeDir,
     trace::{DefaultMakeSpan, TraceLayer},
@@ -30,6 +37,27 @@ use futures::{sink::SinkExt, stream::StreamExt};
 use rustrict::CensorStr;
 use serde_json;
 
+use postgres::Client;
+use lazy_static::lazy_static;
+lazy_static! {
+    static ref DB_CLIENT: Mutex<Client> = Mutex::new(Client::connect("postgres://user:password@localhost/database", postgres::NoTls).unwrap());
+}
+
+static USER_ID: Lazy<Arc<AtomicUsize>> = Lazy::new(|| Arc::new(AtomicUsize::new(0)));
+
+fn main() {
+    GLOBAL_INT.fetch_add(1, Ordering::SeqCst);
+    println!("Global value: {}", GLOBAL_INT.load(Ordering::SeqCst));
+}
+
+// Our shared state
+struct AppState {
+    // We require unique usernames. This tracks which usernames have been taken.
+    user_set: Mutex<HashSet<String>>,
+    // Channel used to send messages to all connected clients.
+    tx: broadcast::Sender<String>,
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::registry()
@@ -42,10 +70,17 @@ async fn main() {
 
     let assets_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
 
+    // Set up application state for use with with_state().
+    let user_set = Mutex::new(HashSet::new());
+    let (tx, _rx) = broadcast::channel(100);
+
+    let app_state = Arc::new(AppState { user_set, tx });
+
     // build our application with some routes
     let app = Router::new()
         .fallback_service(ServeDir::new(assets_dir).append_index_html_on_directories(true))
         .route("/ws", get(ws_handler))
+        .with_state(app_state)
         // logging so we can see whats going on
         .layer(
             TraceLayer::new_for_http()
@@ -74,6 +109,7 @@ async fn ws_handler(
     ws: WebSocketUpgrade,
     user_agent: Option<TypedHeader<headers::UserAgent>>,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    State(state): State<Arc<AppState>>
 ) -> impl IntoResponse {
     let user_agent = if let Some(TypedHeader(user_agent)) = user_agent {
         user_agent.to_string()
@@ -84,11 +120,61 @@ async fn ws_handler(
     println!("`{user_agent}` at {addr} connected.");
     // finalize the upgrade process by returning upgrade callback.
     // we can customize the callback by sending additional info such as address.
-    ws.on_upgrade(move |socket| handle_socket(socket, addr))
+    ws.on_upgrade(move |socket| handle_socket(socket, addr, state))
 }
 
 /// Actual websocket statemachine (one will be spawned per connection)
-async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
+async fn handle_socket(mut socket: WebSocket, who: SocketAddr, state: Arc<AppState>) {
+    let (mut sender, mut receiver) = stream.split();
+    USER_ID.fetch_add(1, Ordering::SeqCst);
+    let username = USER_ID.clone();
+
+    // We subscribe *before* sending the "joined" message, so that we will also
+    // display it to our client.
+    let mut rx = state.tx.subscribe();
+
+    // Now send the "joined" message to all subscribers.
+    let msg = format!("{username} joined.");
+    tracing::debug!("{msg}");     //We need to do this later! I have zero idea how to implement actual usernames...
+    let _ = state.tx.send(msg);
+
+    // Spawn the first task that will receive broadcast messages and send text
+    // messages over the websocket to our client.
+    let mut send_task = tokio::spawn(async move {
+        while let Ok(msg) = rx.recv().await {
+            // In any websocket error, break loop.
+            if sender.send(Message::Text(msg)).await.is_err() {
+                break;
+            }
+        }
+    });
+
+    // Clone things we want to pass (move) to the receiving task.
+    let tx = state.tx.clone();
+
+    // Spawn a task that takes messages from the websocket, prepends the user
+    // name, and sends them to all broadcast subscribers.
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(Message::Text(text))) = receiver.next().await {
+            // Add username before message.
+            let _ = tx.send(format!("{name}: {text}"));
+        }
+    });
+
+    // If any one of the tasks run to completion, we abort the other.
+    tokio::select! {
+        _ = (&mut send_task) => recv_task.abort(),
+        _ = (&mut recv_task) => send_task.abort(),
+    };
+
+    // Send "user left" message (similar to "joined" above).
+    let msg = format!("{username} left.");
+    tracing::debug!("{msg}");
+    let _ = state.tx.send(msg);
+
+    // Remove username from map so new clients can take it again.
+    state.user_set.lock().unwrap().remove(&username);
+}
     while let Some(msg) = socket.recv().await {
         let msg = if let Ok(mut msg) = serde_json::from_str::<User>(msg.expect("we don't know what happened").to_text().expect("can't convert message to string")) {
             msg.msg = msg.msg.censor();
