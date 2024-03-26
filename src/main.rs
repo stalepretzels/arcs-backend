aeb2f4c03c83f16de0638d4387ed8f247d4eb58e
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-03-26 10:28:11 -0400

    fix 6

--- src/main.rs
+++ src/main.rs
@@ -99,7 +99,7 @@ async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
         };
 
         println!(format!("<{}>: {}", message.user, message.msg));
-        if socket.send(Message::Text(serde_json.to_string(msg).expect("cant convert msg to string"))).await.is_err() {
+        if socket.send(Message::Text(serde_json::to_string(msg).expect("cant convert msg to string"))).await.is_err() {
             // client disconnected
             return;
         }
